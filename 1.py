d361c9b5316130c1a10e6bec465e87cc1a22004d
xton94@gmail.com <xton94@gmail.com>
2022-10-13 19:55:25 +0300

    homework 3

---
+++ 1.py
@@ -0,0 +1,14 @@
+a = 5
+
+d = "text"
+
+print(a)
+print(type(a))
+print(d)
+print(type(d))
+
+a = ''' "asssaaa 'sdfdf' " '''
+print(len(a))
+# print(type(a))
+
+
---
+++ Homework_1/Homework_1.py
@@ -0,0 +1,4 @@
+a = input("write text")
+print(a)
+print(a[::2])
+print(a[::-1])
